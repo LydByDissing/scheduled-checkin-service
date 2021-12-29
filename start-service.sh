58b986bcf70922fe2b226393a7d2b3d2fc1f7fa5
Tue S. Dissing <tue@somersault.dk>
2021-12-29 20:04:43 +0100

    not wasting more time on docker-compose

--- start-service.sh
+++ start-service.sh
@@ -1,1 +1,9 @@
-docker-compose up -d
+export SERVICE_NAME=scheduled-checkin-service
+
+docker rm -f $SERVICE_NAME | true
+docker run \
+        -d \
+        --name scheduled-checkin-service \
+        --restart always \
+        --log-driver=json-file --log-opt max-size="10m" --log-opt max-file="10" \
+         $SERVICE_NAME
