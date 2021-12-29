294574d2b4eb109c8293aed634ec0e59fbe6fb95
Tue S. Dissing <tue@somersault.dk>
2021-12-29 19:08:40 +0100

    moving to docker compose

--- start-service.sh
+++ start-service.sh
@@ -1,9 +1,1 @@
-export SERVICE_NAME=scheduled-checkin-service
-
-docker rm -f $SERVICE_NAME | true
-
-docker run \
-        -d \
-        --name scheduled-checkin-service \
-        --restart always \
-         $SERVICE_NAME
+docker-compose up -d
