50050867a74a0ebcb32fa278ddd527bd60aa6c15
Tue S. Dissing <tue@somersault.dk>
2021-12-29 19:00:40 +0100

    correcting build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+export HEARTBEAT_URI=https://receive-heartbeat-service.lydbydissing.workers.dev/heartbeat
+export HEARTBEAT_LOCATION=Copenhagen
+
 exec java -DHEARTBEAT_URI="${HEARTBEAT_URI}" -DHEARTBEAT_LOCATION="${HEARTBEAT_LOCATION}" -jar /bin/scheduled-checkin-service.jar
---
+++ start-service.sh
@@ -0,0 +1,5 @@
+docker run \
+        -d \
+        --name scheduled-checkin-service \
+        --restart always \
+         scheduled-checkin-service
