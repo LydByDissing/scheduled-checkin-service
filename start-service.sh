4380d328dfd67c2aa82af55dd780733be1330c03
Tue S. Dissing <tue@somersault.dk>
2022-01-01 14:46:33 +0100

    eanup

--- start-service.sh
+++
@@ -1,9 +0,0 @@
-export SERVICE_NAME=scheduled-checkin-service
-
-docker rm -f $SERVICE_NAME | true
-docker run \
-        -d \
-        --name scheduled-checkin-service \
-        --restart always \
-        --log-driver=json-file --log-opt max-size="10m" --log-opt max-file="10" \
-         $SERVICE_NAME
